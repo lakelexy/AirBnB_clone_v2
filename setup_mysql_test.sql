9ca5217a42fb8566368c03733a6a398bf9846ce4
lakelexy <alao8380@yahoo.com>
2023-03-23 01:49:01 +0000

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,9 @@
---- Prepares a MySQL Test server for the project.
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+-- Prepares a MySQL server for the hbnb project.
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+USE `hbnb_test_db`;
+
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
+	IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
+	IDENTIFIED BY 'hbnb_test_pwd';
