6d786151b2524db3f46063493d725ec5ca60b901
lakelexy <alao8380@yahoo.com>
2023-04-25 19:11:08 +0000

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,28 @@
-#!/usr/bin/pyhton3
-"""a flask script to display “Hello HBNB!”"""
+#!/usr/bin/python3
+"""
+start Flask application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """a function to return hello HBNB"""
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def index_hbnb():
-    """a function to return HBNB"""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def index_c(text):
-    """a function to return C is fun"""
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
